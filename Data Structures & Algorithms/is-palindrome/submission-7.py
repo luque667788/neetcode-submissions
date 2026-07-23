c25bf08d14a4fcd3533966d478bff9f54a9e9f78
Luiz Mendonca <79766144+luque667788@users.noreply.github.com>
2026-07-23 13:09:28 +0200

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        h = 0
+        t = len(s) -1
+        while True:
+            h_a =  s[h].isalnum()
+            t_a = s[t].isalnum()
+
+            if h_a and t_a:
+                 # go to the next comparison
+
+                if s[h].lower() != s[t].lower():
+                    print(f"head: {s[h]} and the tail is:{s[t]}")
+                    return False
+
+               
+                t -= 1
+                h += 1
+            else:
+                # one of the parties is non alphanumeric, remain in the same comparion (pseudo index)
+                if not h_a:
+                    h += 1
+                if not t_a:
+                    t -= 1
+            if h >= t: #  they will cross always before invalid index (out of bound)
+                    return True
+
+
+
+
+
